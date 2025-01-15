3ce542462b72bcb90547fc1e8edf7d837a350ea2
jony <jony@setuptech.pt>
2025-01-15 12:50:29 +0600

    Ocr convert changed

--- app/Jobs/OcrConvert.php
+++ app/Jobs/OcrConvert.php
@@ -42,28 +42,41 @@ class OcrConvert implements ShouldQueue
 
         $stroage = Storage::put($file_path,base64_decode($this->request['content']));
 
+        $file_path = Str::random('17').".pdf";
+        $converted_file_path = "asset/".Str::random('17').".pdf";
+        //$converted_file_path = public_path('asset/ocr22.pdf');
+
+        $stroage = Storage::put($file_path,base64_decode($this->request['content']));
+
         if (env('APP_ENV') != 'pc'){
-            $cmd= "ocrmypdf ".storage_path("app/".$file_path).' '.public_path($converted_file_path).' --skip-text --optimize 0';
-            $process = Process::timeout(900)->start($cmd);
+//            $cmd= "/usr/local/bin/ocrmypdf ".storage_path("app/".$file_path).' '.public_path($converted_file_path).' --skip-text --optimize 0';
+//
+//            $cmd = "python azure_ocr.py /Users/jony/projects/chatpdfbackend/app/Console/Commands/Python/pdf/deposito.pdf https://ocr-intelligence-azure.cognitiveservices.azure.com/ 9FH3vVnU1Gzh2A3p0gsJaVIYRqUeiqkC7rj7dPUoI3UmoEky4O6rJQQJ99BAACYeBjFXJ3w3AAALACOG8b30";
+//            $process = Process::timeout(900)->start($cmd);
+//
+//
 
-            while ($process->running()) {
-//                 Log::info($process->latestOutput());
-//                  Log::info($process->latestErrorOutput());
 
-                sleep(1);
-            }
-            $result = $process->wait();
 
-            if ($result->successful()) {
+            $pythonVenv = '/var/www/admin.xtiplyai.com/html/app/Console/Commands/Python/venv/bin/activate'; // Path to your venv
+//        $script_path = "/Users/jony/projects/mupdf/file_to_image.py"; // Path to your venv
+            $script_path = '/var/www/admin.xtiplyai.com/html/app/Console/Commands/Python/azure_ocr.py'; // Path to your venv
+            $azure_end_point = env('AZURE_OCR_ENDPOINT');
+            $azure_secrect_key = env('AZURE_OCR_KEY');
+            //  $command = "source $pythonVenv && python3 $script_path " .escapeshellarg($apiKey)." ". escapeshellarg($inputPdfs) . " " . escapeshellarg($outputDir) . " " . escapeshellarg($openaiModel) ;
+            $command = "bash -c 'source $pythonVenv && python3 $script_path " . escapeshellarg(storage_path("app/".$file_path)) . " " . escapeshellarg($azure_end_point) . " " . escapeshellarg($azure_secrect_key) . " && deactivate' 2>&1";
+
+            $output = shell_exec($command);
+
+            $converted_file_path = str_replace("\n", "",$output);
+
+            if (file_exists($converted_file_path)) {
                 $data = $this->request;
-                $data['content'] =base64_encode(file_get_contents(public_path($converted_file_path)));
+                $data['content'] =base64_encode(file_get_contents($converted_file_path));
                 $response = Http::timeout(900)->post($this->request['callback'], $data);
-                Log::info("incoming");
-                Log::info(json_encode($response->body()));
-                Log::info(json_encode('success ocr'));
+
 
             }else{
-                Log::info(json_encode("error ocr=".$result->errorOutput()));
                 $data = $this->request;
                 $data['content'] =base64_encode(file_get_contents(storage_path("app/".$file_path)));
                 $response = Http::timeout(900)->post($this->request['callback'], $data);
